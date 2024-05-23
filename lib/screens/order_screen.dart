b81c0987b338fc199cfa3a274b88336f65881231
Pratik Adsare <pratikadsare70@gmail.com>
2024-05-24 00:45:07 +0530

    resolved error

--- lib/screens/order_screen.dart
+++ lib/screens/order_screen.dart
@@ -1,31 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_application_rajeshwari/Providers/orders_provider/orders.dart';
-// import 'package:flutter_application_rajeshwari/widgets/order_item.dart';
-// import 'package:provider/provider.dart';
-// // import 'package:flutter_provider_shop/Providers/orders_provider/orders.dart';
-// // import 'package:flutter_provider_shop/widgets/order_item.dart';
-// // import 'package:provider/provider.dart';
-
-// class OrdersScreen extends StatelessWidget {
-//   static const id = '/OrdersScreen';
-//   const OrdersScreen({Key? key}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     final orders = Provider.of<Orders>(context);
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Your Orders'),
-//       ),
-//       body: ListView.builder(
-//           itemCount: orders.myOrders.length,
-//           itemBuilder: (context, index) => OrderItemTile(
-//                 order: orders.myOrders[index],
-//               )),
-//     );
-//   }
-// }
-
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
